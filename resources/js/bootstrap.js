9ba6eb7b656ba00135515ef298f44df56cebb65c
Lakshan Madushanka <epmadushanka@gmail.com>
2023-05-21 17:19:18 +0530

    Fix Laravel Echo not found

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,13 +9,6 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-// Alpine js
-import Alpine from 'alpinejs'
-window.Alpine = Alpine
-
-Alpine.start()
-
-
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
