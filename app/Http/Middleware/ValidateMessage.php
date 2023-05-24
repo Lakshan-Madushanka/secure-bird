cc496b69f587ed327d39058b94ad627a9db1af87
Lakshan Madushanka <epmadushanka@gmail.com>
2023-05-24 17:01:12 +0530

    ValidateMessage Middleware

---
+++ app/Http/Middleware/ValidateMessage.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use App\Models\Message;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ValidateMessage
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $isValid = Message::query()
+            ->whereId($request->get('messageId'))
+            ->valid()
+            ->exists();
+
+        if ( ! $isValid) {
+            abort(404);
+        }
+
+        return $next($request);
+    }
+}
---
+++ tests/Feature/Middlewares/ValidateMessageMiddlewareTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+
+use App\Http\Middleware\ValidateMessage;
+use App\Models\Message;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Str;
+
+it('throws route not found exception for invalid message id', function (): void {
+    $messageId = Str::uuid()->toString();
+
+    $request = Request::create(
+        uri: route('messages.show', ['messageId' => $messageId]),
+        parameters: ['messageId' => $messageId]
+    );
+
+    $middleware = new ValidateMessage();
+    $requestSucceeded = false;
+
+    $middleware->handle($request, static function () use (&$requestSucceeded) {
+        $requestSucceeded = true;
+        return new \Symfony\Component\HttpFoundation\Response();
+    });
+})->throws(NotFoundHttpException::class);
+
+it('allows to proceed route with valid message id', function (): void {
+    $messageId = Message::factory()->create(['expires_at' => now()->addMinute(5)])->id;
+
+    $request = Request::create(
+        uri: route('messages.show', ['messageId' => $messageId]),
+        parameters: ['messageId' => $messageId]
+    );
+
+    $middleware = new ValidateMessage();
+    $requestSucceeded = false;
+
+    $middleware->handle($request, static function () use (&$requestSucceeded) {
+        $requestSucceeded = true;
+        return new \Symfony\Component\HttpFoundation\Response();
+    });
+
+    expect($requestSucceeded)->toBeTrue();
+});
